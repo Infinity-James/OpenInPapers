38b54d05d1ac4aee51966e4d939679f30663ee58
James Valaitis <james.valaitis@springer.com>
2014-07-21 11:35:06 +0100

    Initial commit.

---
+++ OpenInPapers/App Delegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  OpenInPapersExtension
+//
+//  Created by James Valaitis on 21/07/2014.
+//  Copyright (c) 2014 James Valaitis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+	var window: UIWindow?
+
+
+	func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool
+	{
+		return true
+	}
+}
---
+++ OpenInPapers/Controller/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  OpenInPapersExtension
+//
+//  Created by James Valaitis on 21/07/2014.
+//  Copyright (c) 2014 James Valaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+}
